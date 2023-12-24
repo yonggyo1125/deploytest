6ce1e4b262edbab9528fa59ae55900404e42d4d5
yonggyo1125 <yonggyo00@kakao.com>
2023-12-24 16:18:25 +0900

    테스트

---
+++ src/main/java/org/project/DeploytestApplication.java
@@ -0,0 +1,13 @@
+package org.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project/DeploytestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
