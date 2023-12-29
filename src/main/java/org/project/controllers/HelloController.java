6bb92a160ac4496c9db76708377b4884be3cb24a
LEE, YONGGYO <103939561+yonggyo1125@users.noreply.github.com>
2023-12-29 09:06:37 +0900

    Update HelloController.java

--- src/main/java/org/project/controllers/HelloController.java
+++ src/main/java/org/project/controllers/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "안녕";
+        return "안녕하세요";
     }
 }
